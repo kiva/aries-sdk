16b71a537d4e89bc0a445e32ea66e669e0630c85
Camilo Parra <caeparra@gmail.com>
2019-09-17 13:24:18 -0600

    lib structure added as well as some dependencies and error codes to accelerate arch process
    Signed-off-by: Camilo Parra <caeparra@gmail.com>

---
+++ libaries/src/api/wallet.rs
@@ -0,0 +1,20 @@
+use libc::c_char;
+
+
+
+#[no_mangle]
+pub extern fn aries_register_wallet_storage(){}
+#[no_mangle]
+pub extern fn aries_create_wallet(){}
+#[no_mangle]
+pub extern fn aries_open_wallet(){}
+#[no_mangle]
+pub extern fn aries_export_wallet(){}
+#[no_mangle]
+pub extern fn aries_import_wallet(){}
+#[no_mangle]
+pub extern fn aries_close_wallet(){}
+#[no_mangle]
+pub extern fn aries_delete_wallet(){}
+#[no_mangle]
+pub extern fn aries_generate_wallet_key(){}
--- libaries/src/lib.rs
+++ libaries/src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+extern crate futures;
+
+extern crate rmp_serde;
+
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[macro_use]
+extern crate serde_json;
+
+
+
+
